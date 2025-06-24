9c5c0e6d280808c001afcf4d6e414ddc5baddf2f
Anton Knysh <anton.knysh@andevart.com>
2025-06-24 08:56:09 +0300

    added configs to the "frontend" part

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,28 +1,118 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import { defineConfig } from "eslint/config";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+import importPlugin from "eslint-plugin-import";
+import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+
+export default defineConfig([
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ["node_modules/**", "dist/**", "build/**", "**/*.d.ts"],
+  },
+
+  // JS/JSX config
+  {
+    files: ["**/*.js", "**/*.jsx"],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2024,
+      },
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      import: importPlugin,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: false,
+            orderImportKind: "asc",
+          },
+        },
       ],
+      semi: ["error", "always"],
+      quotes: ["off", "single", { allowTemplateLiterals: true }],
+      "comma-dangle": ["error", "always-multiline"],
+      "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+
+      "no-irregular-whitespace": ["error", { skipTemplates: true, skipStrings: true }],
+    },
+    settings: {
+      react: { version: "detect" },
     },
   },
-)
+
+  // TS/TSX config
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["./tsconfig.app.json", "./tsconfig.node.json"],
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.es2024,
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+      react: reactPlugin,
+      "react-hooks": reactHooksPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      ...reactHooksPlugin.configs.recommended.rules,
+
+      "import/order": [
+        "error",
+        {
+          groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+          alphabetize: {
+            order: "asc",
+            caseInsensitive: false,
+            orderImportKind: "asc",
+          },
+        },
+      ],
+
+      semi: ["error", "always"],
+      quotes: ["off", "single", { allowTemplateLiterals: true }],
+      "comma-dangle": ["error", "always-multiline"],
+      "no-console": process.env.NODE_ENV === "production" ? "error" : "warn",
+      "no-unused-vars": "off",
+      "no-undef": "off",
+
+      "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/no-non-null-assertion": "off",
+
+      "react/react-in-jsx-scope": "off",
+      "react/prop-types": "off",
+
+      "no-irregular-whitespace": ["error", { skipTemplates: true, skipStrings: true }],
+    },
+    settings: {
+      react: { version: "detect" },
+    },
+  },
+]);
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from 'node:path';
+
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import autoprefixer from 'autoprefixer';
+import { defineConfig } from 'vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    legacy({
+      targets: ['> 0.01%'],
+    }),
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+      },
+    },
+  },
+  css: {
+    postcss: {
+      plugins: [autoprefixer({})],
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
+});
